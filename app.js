e5c7b17f2aa9f1c825b1fed40ca5c28901a531f8
thenewsoursxxx-del <thenewsoursxxx@gmail.com>
2026-01-27 13:32:02 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -394,16 +394,14 @@ document.querySelectorAll(".input-wrap input").forEach(input => {
 
   input.addEventListener("focus", () => {
     wrap.classList.remove("error", "shake");
-    wrap.classList.add("show-hint"); // ← ВОТ ЧЕГО НЕ ХВАТАЛО
+
+    if (input.dataset.placeholder) {
+      input.placeholder = input.dataset.placeholder;
+    }
   });
 
   input.addEventListener("input", () => {
     wrap.classList.remove("error", "shake");
-    wrap.classList.remove("show-hint");
-  });
-
-  input.addEventListener("blur", () => {
-    wrap.classList.remove("show-hint");
   });
 });
 
