d78155f2a55f869458f7db6877d9cf1b1e4ec04d
thenewsoursxxx-del <thenewsoursxxx@gmail.com>
2026-01-28 19:46:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -58,8 +58,6 @@ const sheetOverlay = document.getElementById("sheetOverlay");
 const noBuffer = document.getElementById("noBuffer");
 const withBuffer = document.getElementById("withBuffer");
 
-const calcLock = document.getElementById("calcLock");
-const lockText = document.getElementById("lockText");
 const resetBtn = document.getElementById("resetPlan");
 
 const confirmReset = document.getElementById("confirmReset");
