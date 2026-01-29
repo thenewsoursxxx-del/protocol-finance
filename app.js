4e20daec719102d9e8e2bd21b83a7a779788b0f7
thenewsoursxxx-del <thenewsoursxxx@gmail.com>
2026-01-29 13:26:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -225,18 +225,20 @@ ${advice.text}
 
 document.querySelectorAll(".scenario-card").forEach(card => {
 card.onclick = () => {
-  document
-    .querySelectorAll(".scenario-card")
-    .forEach(c => c.classList.remove("active"));
+document
+.querySelectorAll(".scenario-card")
+.forEach(c => c.classList.remove("active"));
 
-  card.classList.add("active");
+card.classList.add("active");
 
-  selectedScenario = card.dataset.id;
+selectedScenario = card.dataset.id;
 
-  haptic("light");
+haptic("light");
 
-  protocolFlow(selectedScenario);
+protocolFlow(selectedScenario);
 };
+});
+}
 
 /* ===== CALCULATE ===== */
 calculateBtn.onclick = () => {
