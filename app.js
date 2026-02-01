a7c081acd0ecd2ca288514fb3ea695c57b8231d2
thenewsoursxxx-del <thenewsoursxxx@gmail.com>
2026-02-01 18:48:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -691,6 +691,13 @@ if (hit) {
 });
 }
 
+document.addEventListener("click", e => {
+  if (!e.target.closest("canvas")) {
+    activeFactDot = null;
+    drawChart();
+  }
+});
+
 function drawChart() {
 // ===== GROUP FACTS BY MONTH (SAFE) =====
 const groupedFacts = {};
